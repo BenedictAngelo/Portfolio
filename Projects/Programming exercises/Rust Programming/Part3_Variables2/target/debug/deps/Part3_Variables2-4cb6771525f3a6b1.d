c9bc04862121f2ee7e4abdb018169cea5a9f24dd
Benedict Angelo <BenedictAngelo@protonmail.com>
2026-01-20 14:19:11 +0400

    Nothing

---
+++ Projects/Programming exercises/Rust Programming/Part3_Variables2/target/debug/deps/Part3_Variables2-4cb6771525f3a6b1.d
@@ -0,0 +1,5 @@
+/home/angelo/Github clones/Portfolio/Projects/Programming exercises/Rust Programming/Part3_Variables2/target/debug/deps/Part3_Variables2-4cb6771525f3a6b1.d: src/main.rs
+
+/home/angelo/Github clones/Portfolio/Projects/Programming exercises/Rust Programming/Part3_Variables2/target/debug/deps/libPart3_Variables2-4cb6771525f3a6b1.rmeta: src/main.rs
+
+src/main.rs:
